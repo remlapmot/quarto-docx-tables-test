ea982cb6e014d8d63c9a9a5f1c4bb865861157c9
Tom Palmer <remlapmot@hotmail.com>
2024-03-04 15:06:36 +0000

    Update render.R

--- render.R
+++ render.R
@@ -3,3 +3,5 @@ quarto::quarto_render("index.qmd", output_format = "docx")
 quarto::quarto_render("index.qmd", output_format = "html")
 
 quarto::quarto_render("index.qmd", output_format = "pdf")
+
+rmarkdown::render("index-bookdown.Rmd")
